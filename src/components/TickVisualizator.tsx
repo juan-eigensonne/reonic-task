728e4021d4f5e191dbed34a13f01dfc1425b7dfd
Juan David Zapata Sánchez <juan.zapatasanchez@eigensonne.de>
2024-09-26 15:30:04 +0200

    Updating the tick visualizator with icon and smaller view

--- src/components/TickVisualizator.tsx
+++ src/components/TickVisualizator.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { TickResponse } from 'reonic';
 
 export type TickVisualizatorProps = {
@@ -13,12 +14,10 @@ export function TickVisualizator(props: TickVisualizatorProps) {
     } = props.data;
     return (
         <div className="mt-8">
-            <h1 className="text-4xl font-bold tracking-tight">
-                One Tick Run: {time}
-            </h1>
-            <div className="flex flex-wrap justify-around">
+            <h1 className="text-xl font-bold tracking-tight">Run: {time}</h1>
+            <div className="flex flex-wrap justify-around gap-1-px">
                 {updatedChargePointList.map(
-                    ({ busyTimeStart, busyTimeEnd }) => {
+                    ({ busyTimeStart, busyTimeEnd }, index) => {
                         const isBusy: boolean =
                             !!busyTimeStart &&
                             !!busyTimeEnd &&
@@ -26,25 +25,35 @@ export function TickVisualizator(props: TickVisualizatorProps) {
                             busyTimeEnd >= 0;
                         return (
                             <div
-                                className={`border-solid border-2 ${isBusy ? 'bg-red-300' : ''} w-1/5`}
+                                key={isBusy + '_' + index}
+                                className={`border-solid border-2 ${isBusy ? 'bg-yellow-300' : ''} min-h-8 min-w-8 flex align-middle justify-around `}
                             >
-                                <p>
-                                    busyTimeStart:{' '}
-                                    {busyTimeStart ? busyTimeStart : 0}
-                                </p>
-                                <p>
-                                    busyTimeEnd: {busyTimeEnd ? busyTimeEnd : 0}
-                                </p>
+                                {isBusy && (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="size-6"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+                                        />
+                                    </svg>
+                                )}
                             </div>
                         );
                     },
                 )}
             </div>
 
-            <h1 className="text-4xl font-bold tracking-tight">
+            <h1 className="text-xl font-bold tracking-tight">
                 pwerDemanded: {powerDemanded}
             </h1>
-            <h1 className="text-4xl font-bold tracking-tight">
+            <h1 className="text-xl font-bold tracking-tight">
                 consumedEnergyInTick: {consumedEnergyInTick}
             </h1>
         </div>
