0274f60b7a2a7c50819f737b6d3ffdd452d8fdfe
Juan David Zapata Sánchez <juan.zapatasanchez@eigensonne.de>
2024-09-26 15:29:37 +0200

    Updating the layout and index to use the SimulationResult component

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -7,38 +7,24 @@ import { Footer } from '../components/footer';
 
 type RootLayoutProps = { children: ReactNode };
 
-export default async function RootLayout({ children }: RootLayoutProps) {
-  const data = await getData();
-
-  return (
-    <html>
-      <head></head>
-      <body>
-        <div className="font-['Nunito']">
-          <meta property="description" content={data.description} />
-          <link rel="icon" type="image/png" href={data.icon} />
-          <Header />
-          <main className="m-6 flex items-center *:min-h-64 *:min-w-64 lg:m-0 lg:min-h-svh lg:justify-center">
-            {children}
-          </main>
-          <Footer />
-        </div>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+        <html>
+            <body>
+                <div className="font-['Nunito']">
+                    <Header />
+                    <main className="m-6 flex items-center *:min-h-64 *:min-w-64 lg:m-0 lg:min-h-svh lg:justify-center">
+                        {children}
+                    </main>
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    );
 }
 
-const getData = async () => {
-  const data = {
-    description: 'An internet website!',
-    icon: '/images/favicon.png',
-  };
-
-  return data;
-};
-
 export const getConfig = async () => {
-  return {
-    render: 'static',
-  };
+    return {
+        render: 'static',
+    };
 };
